369bfcf03c07a0b020c124a6ee3995abb7ab3526
chandudevops <imsekhardeul@gmail.com>
2019-03-24 11:27:12 +0000

    newcode

--- demo.java
+++ demo.java
@@ -3,6 +3,10 @@ public class demojava
   public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
-    } 
+    }
+      public demojava()
+       {
+         System.out.println("I'm constructor");  
+       } 
 }
 
